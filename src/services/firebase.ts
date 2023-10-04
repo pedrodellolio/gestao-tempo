e6c2e9f2045005554229e14d81a4845958259721
pedrodellolio <pedrohdellolio@gmail.com>
2023-10-04 20:41:56 -0300

    Alteração nas env variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.APP_KEY_FIREBASE,
-  authDomain: process.env.AUTH_DOMAIN_FIREBASE,
-  projectId: process.env.PROJECT_ID_FIREBASE,
-  storageBucket: process.env.STORAGE_BUCKET_FIREBASE,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID_FIREBASE,
-  appId: process.env.APP_ID_FIREBASE,
-  measurementId: process.env.MEASUREMENT_ID_FIREBASE,
+  apiKey: import.meta.env.VITE_APP_KEY_FIREBASE,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN_FIREBASE,
+  projectId: import.meta.env.VITE_PROJECT_ID_FIREBASE,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID_FIREBASE,
+  appId: import.meta.env.VITE_APP_ID_FIREBASE,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID_FIREBASE,
 };
 
 const app = initializeApp(firebaseConfig);
