a7755e902a43edae3536ab9213af5112bf8d739e
pedrodellolio <pedrohdellolio@gmail.com>
2023-10-04 20:18:13 -0300

    Atualização variáveis firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,16 +3,16 @@ import { getAnalytics } from "firebase/analytics";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.APP_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.APP_KEY_FIREBASE,
+  authDomain: process.env.AUTH_DOMAIN_FIREBASE,
+  projectId: process.env.PROJECT_ID_FIREBASE,
+  storageBucket: process.env.STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID_FIREBASE,
+  appId: process.env.APP_ID_FIREBASE,
+  measurementId: process.env.MEASUREMENT_ID_FIREBASE,
 };
 
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
