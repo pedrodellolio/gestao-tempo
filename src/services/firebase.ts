5466cd2812d21e37de6af4f512336eb98f7f1ceb
pedrodellolio <pedrohdellolio@gmail.com>
2023-10-04 21:01:38 -0300

    Correção variáveis firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_KEY_FIREBASE,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN_FIREBASE,
-  projectId: import.meta.env.VITE_PROJECT_ID_FIREBASE,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET_FIREBASE,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID_FIREBASE,
-  appId: import.meta.env.VITE_APP_ID_FIREBASE,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID_FIREBASE,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
