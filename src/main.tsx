c22b665ad76870d09fae83f606d216e49d894301
pedrodellolio <pedrohdellolio@gmail.com>
2024-09-26 14:28:03 -0300

    Versioning untracked files

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import "./index.css";
 import AuthRoot from "./routes/auth/auth-root";
 import Login from "./routes/auth/login";
 import Register from "./routes/auth/register";
-import Home from "./routes/home";
-import Pomodoro from "./routes/pomodoro";
 import PrivateRoot from "./routes/private-root";
 import Progress from "./routes/progress";
 import React from "react";
@@ -13,6 +11,8 @@ import { PreferencesProvider } from "./context/preferences-context";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/sonner";
+import Home from "./routes/home";
+import Pomodoro from "./routes/pomodoro";
 
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
